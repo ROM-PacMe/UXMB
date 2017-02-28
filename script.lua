18412e7ad2df5ae32df9360696204f0ae48c955c
Rafael PacMe <rafy.pachecomendez@gmail.com>
2017-02-28 04:26:43 -0300

    add initial tree of UXMB | PSP ONLY Signed and Created by: @ROM-PacMe

---
+++ script.lua
@@ -0,0 +1,18 @@
+---------------------------------------------------------------
+
+-- Welcome to the Main Script of UXMB: PS4 Edition.
+-- Developer(s): -ROM-PacMe
+--               -KillingName
+
+---------------------------------------------------------------
+---------------------------------------------------------------
+
+-- Bienvenido al Script Principal de UXMB: PS4 Edition.
+-- Desarrollador(es): -ROM-PacMe
+--                    -KillingName
+
+---------------------------------------------------------------
+
+-- ## Enter to Recovery Mode ## 
+-- ## Entrar al Modo Recovery / Recuperación ##
+dofile("uxmb_recovery.lua")
